d96af7b9b6645c98dceb6a3604d1cf53a4db6c50
Xavier Daste <xavier.daste@epitech.eu>
2025-11-19 19:14:00 +0100

    feat(engine): add components to help with rendering sprites, animation and camera

---
+++ Client/src/engine/graphics/RenderComponents.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <SDL2/SDL.h>
+
+#include <string>
+
+#include "Physics2D.hpp"
+
+struct Sprite {
+  std::string textureKey;
+  SDL_Rect sourceRect;
+  Vector2 pivot;  // 0,0 = top left, 0.5,0.5 = center
+  int layer;
+
+  Sprite(const std::string& key, SDL_Rect src = {0, 0, 0, 0},
+         Vector2 piv = {0.5f, 0.5f}, int lyr = 0)
+      : textureKey(key), sourceRect(src), pivot(piv), layer(lyr) {}
+};
+
+struct Animation {
+  std::string currentAnimation;
+  float currentTime;
+  int currentFrame;
+  bool isPlaying;
+
+  Animation(const std::string& anim = "", bool playing = false)
+      : currentAnimation(anim),
+        currentTime(0),
+        currentFrame(0),
+        isPlaying(playing) {}
+};
+
+struct Camera {
+  Vector2 position;
+  float zoom;
+  bool isActive;
+
+  Camera(Vector2 pos = {0, 0}, float z = 1.0f, bool active = true)
+      : position(pos), zoom(z), isActive(active) {}
+};
