bd92c6de54e2b4e49b84fd34d5e3517cd4628524
Xavier Daste <xavier.daste@epitech.eu>
2025-11-19 19:12:45 +0100

    feat(engine): add AnimationManager class to handle sprite animations using textures

---
+++ Client/src/engine/graphics/AnimationManager.cpp
@@ -0,0 +1,16 @@
+#include "AnimationManager.hpp"
+
+void AnimationManager::CreateAnimation(
+    const std::string& name, const std::string& textureKey,
+    const std::vector<AnimationFrame>& frames, bool loop) {
+  AnimationClip clip(loop);
+  clip.textureKey = textureKey;
+  clip.frames = frames;
+  m_animations[name] = clip;
+}
+
+const AnimationClip* AnimationManager::GetAnimation(
+    const std::string& name) const {
+  auto it = m_animations.find(name);
+  return (it != m_animations.end()) ? &it->second : nullptr;
+}
