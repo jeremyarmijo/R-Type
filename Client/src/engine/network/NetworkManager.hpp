bafc6265f04dbfae9b0e234366a5dd90e67df77e
jasonzohou <jason.zohou@epitech.eu>
2025-11-25 14:13:02 +0100

    feat(network): add first achitechture of client network with connexion of TCP in thread

--- Client/src/engine/network/NetworkManager.hpp
+++ Client/src/engine/network/NetworkManager.hpp
@@ -1,9 +1,52 @@
 #pragma once
+#include <string>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include "CircularBuffer.hpp"
+#include "Event.hpp"
 
 class NetworkManager {
- private:
- public:
-  NetworkManager() {}
 
-  bool IsConnected() const { return false; }
+private:
+    bool connected = false;
+    bool tcpConnected = false;
+    bool udpConnected = false;
+
+    bool running = false;
+
+    int tcpSocket = -1;
+    int tcpPort = -1;
+    int udpPort = -1;
+    int udpSocket = -1;
+    std::string serverIP;
+
+    std::thread networkThread;
+
+    int ConnectTCP();
+    int ConnectUDP(){};
+
+    void ReadTCP();
+    void ReadUDP(){};
+
+    std::vector<uint8_t> recvBuffer;
+    void ProcessRecvBuffer();
+
+    Event DecodePacket(std::vector<uint8_t>& packet);
+    CircularBuffer<Event> eventBuffer;
+
+public:
+    NetworkManager::NetworkManager() : eventBuffer(10) {};
+    ~NetworkManager();
+
+    bool Connect(const std::string& ip, int port);
+    void Disconnect();
+
+    bool IsConnected() const { return connected; }
+
+    void ThreadLoop();
+
+    void SendInput();
+
+    Event PopEvent();
 };
