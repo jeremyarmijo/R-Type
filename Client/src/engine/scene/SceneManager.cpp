3da3ea53b87903091a14b70ba45982b0b1e2396a
Xavier Daste <xavier.daste@epitech.eu>
2025-11-26 03:44:56 +0100

    feat(engine): scene manager class to handle swapping between game scenes and handling per scene logic

---
+++ Client/src/engine/scene/SceneManager.cpp
@@ -0,0 +1,53 @@
+#include "scene/SceneManager.hpp"
+
+#include "engine/GameEngine.hpp"
+
+void SceneManager::ChangeScene(const std::string& name) {
+  auto it = m_scenes.find(name);
+  if (it != m_scenes.end()) {
+    m_nextScene = it->second.get();
+  }
+}
+
+void SceneManager::Update(float deltaTime) {
+  if (m_nextScene && m_nextScene != m_currentScene) {
+    if (m_currentScene) {
+      m_currentScene->OnExit();
+    }
+    m_currentScene = m_nextScene;
+    m_currentScene->OnEnter();
+    m_nextScene = nullptr;
+  }
+
+  if (m_currentScene) {
+    m_currentScene->Update(deltaTime);
+  }
+}
+
+void SceneManager::Render() {
+  if (m_currentScene) {
+    m_currentScene->Render();
+  }
+}
+
+void SceneManager::HandleEvent(SDL_Event& event) {
+  if (m_currentScene) {
+    m_currentScene->HandleEvent(event);
+  }
+}
+
+Registry& Scene::GetRegistry() { return m_engine->GetRegistry(); }
+
+TextureManager& Scene::GetTextures() { return m_engine->GetTextureManager(); }
+
+AnimationManager& Scene::GetAnimations() {
+  return m_engine->GetAnimationManager();
+}
+
+InputManager& Scene::GetInput() { return m_engine->GetInputManager(); }
+
+void Scene::ChangeScene(const std::string& sceneName) {
+  if (m_sceneManager) {
+    m_sceneManager->ChangeScene(sceneName);
+  }
+}
---
+++ Client/src/engine/scene/SceneManager.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#include <SDL2/SDL.h>
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "ecs/Registry.hpp"
+#include "graphics/AnimationManager.hpp"
+#include "graphics/TextureManager.hpp"
+#include "inputs/InputManager.hpp"
+
+class GameEngine;
+class SceneManager;
+
+class Scene {
+ protected:
+  GameEngine* m_engine;
+  SceneManager* m_sceneManager;
+  std::string m_name;
+
+ public:
+  Scene(GameEngine* engine, SceneManager* sceneManager, const std::string& name)
+      : m_engine(engine), m_sceneManager(sceneManager), m_name(name) {}
+
+  virtual ~Scene() {}
+  virtual void OnEnter() = 0;
+  virtual void OnExit() = 0;
+  virtual void Update(float deltaTime) {}
+  virtual void Render() {}
+  virtual void HandleEvent(SDL_Event& event) {}
+  const std::string& GetName() const { return m_name; }
+
+ protected:
+  Registry& GetRegistry();
+  TextureManager& GetTextures();
+  AnimationManager& GetAnimations();
+  InputManager& GetInput();
+
+  void ChangeScene(const std::string& sceneName);
+};
+
+class SceneManager {
+ private:
+  std::unordered_map<std::string, std::unique_ptr<Scene>> m_scenes;
+  Scene* m_currentScene;
+  Scene* m_nextScene;
+
+ public:
+  SceneManager() : m_currentScene(nullptr), m_nextScene(nullptr) {}
+
+  template <typename T, typename... Args>
+  void RegisterScene(const std::string& name, Args&&... args) {
+    m_scenes[name] = std::make_unique<T>(std::forward<Args>(args)...);
+  }
+
+  void ChangeScene(const std::string& name);
+  void Update(float deltaTime);
+  void Render();
+  void HandleEvent(SDL_Event& event);
+  Scene* GetCurrentScene() { return m_currentScene; }
+};
