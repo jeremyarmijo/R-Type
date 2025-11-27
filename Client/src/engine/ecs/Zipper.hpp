c8d44b551e7784343fb96ac475d6008ae65937e3
Xavier Daste <xavier.daste@epitech.eu>
2025-11-27 05:47:09 +0100

    fix(engine): (Index)ZipperIterator now correctly iterates through sparse arrays and registry now correctly adds components/indexs to sparse arrays

--- Client/src/engine/ecs/Zipper.hpp
+++ Client/src/engine/ecs/Zipper.hpp
@@ -12,14 +12,8 @@ template <class... Containers>
 class ZipperIterator {
  private:
   template <class Container>
-  using iterator_t = decltype(std::declval<Container&>().begin());
-
-  template <class Container>
-  using it_reference_t = typename iterator_t<Container>::reference;
-
-  template <class Container>
   using component_t = typename std::remove_reference_t<
-      decltype(std::declval<it_reference_t<Container>>().value())>;
+      decltype(std::declval<Container&>()[0].value())>;
 
  public:
   using value_type = std::tuple<component_t<Containers>&...>;
@@ -27,14 +21,14 @@ class ZipperIterator {
   using pointer = void;
   using difference_type = std::ptrdiff_t;
   using iterator_category = std::forward_iterator_tag;
-  using iterator_tuple = std::tuple<iterator_t<Containers>...>;
+  using container_tuple = std::tuple<std::reference_wrapper<Containers>...>;
 
   friend Zipper<Containers...>;
 
  private:
-  ZipperIterator(iterator_tuple const& it_tuple, size_t max)
-      : m_current(it_tuple), m_max(max), m_idx(0) {
-    if (!all_set(m_seq)) {
+  ZipperIterator(container_tuple const& containers, size_t max, size_t idx = 0)
+      : m_containers(containers), m_max(max), m_idx(idx) {
+    if (m_idx < m_max && !all_set(m_seq)) {
       advance_to_next_valid();
     }
   }
@@ -43,14 +37,14 @@ class ZipperIterator {
   ZipperIterator(const ZipperIterator&) = default;
   ZipperIterator& operator=(const ZipperIterator&) = default;
 
-  // Pre-increment: ++it
   ZipperIterator& operator++() {
-    incr_all(m_seq);
-    advance_to_next_valid();
+    ++m_idx;
+    if (m_idx < m_max) {
+      advance_to_next_valid();
+    }
     return *this;
   }
 
-  // Post-increment: it++
   ZipperIterator operator++(int) {
     ZipperIterator tmp = *this;
     ++(*this);
@@ -71,30 +65,28 @@ class ZipperIterator {
 
  private:
   template <size_t... Is>
-  void incr_all(std::index_sequence<Is...>) {
-    m_idx++;
-    (++std::get<Is>(m_current), ...);
-  }
-
-  template <size_t... Is>
   bool all_set(std::index_sequence<Is...>) {
     if (m_idx >= m_max) return false;
-    return (std::get<Is>(m_current)->has_value() && ...);
+    auto& containers_tuple = m_containers;
+    return ((m_idx < std::get<Is>(containers_tuple).get().size() &&
+             std::get<Is>(containers_tuple).get()[m_idx].has_value()) &&
+            ...);
   }
 
   template <size_t... Is>
   value_type to_value(std::index_sequence<Is...>) {
-    return std::tie(std::get<Is>(m_current)->value()...);
+    auto& containers_tuple = m_containers;
+    return std::tie(std::get<Is>(containers_tuple).get()[m_idx].value()...);
   }
 
   void advance_to_next_valid() {
     while (m_idx < m_max && !all_set(m_seq)) {
-      incr_all(m_seq);
+      ++m_idx;
     }
   }
 
  private:
-  iterator_tuple m_current;
+  container_tuple m_containers;
   size_t m_max;
   size_t m_idx;
   static constexpr std::index_sequence_for<Containers...> m_seq{};
@@ -104,33 +96,22 @@ template <class... Containers>
 class Zipper {
  public:
   using iterator = ZipperIterator<Containers...>;
-  using iterator_tuple = typename iterator::iterator_tuple;
+  using container_tuple = typename iterator::container_tuple;
 
   explicit Zipper(Containers&... cs)
-      : m_begin(compute_begin(cs...)),
-        m_end(compute_end(cs...)),
-        m_size(compute_size(cs...)) {}
+      : m_containers(std::ref(cs)...), m_size(compute_size(cs...)) {}
 
-  iterator begin() { return iterator(m_begin, m_size); }
+  iterator begin() { return iterator(m_containers, m_size, 0); }
 
-  iterator end() { return iterator(m_end, m_size); }
+  iterator end() { return iterator(m_containers, m_size, m_size); }
 
  private:
   static size_t compute_size(Containers&... containers) {
     return std::max({containers.size()...});
   }
 
-  static iterator_tuple compute_begin(Containers&... containers) {
-    return std::make_tuple(containers.begin()...);
-  }
-
-  static iterator_tuple compute_end(Containers&... containers) {
-    return std::make_tuple(containers.end()...);
-  }
-
  private:
-  iterator_tuple m_begin;
-  iterator_tuple m_end;
+  container_tuple m_containers;
   size_t m_size;
 };
 
@@ -138,14 +119,8 @@ template <class... Containers>
 class IndexedZipperIterator {
  private:
   template <class Container>
-  using iterator_t = decltype(std::declval<Container&>().begin());
-
-  template <class Container>
-  using it_reference_t = typename iterator_t<Container>::reference;
-
-  template <class Container>
   using component_t = typename std::remove_reference_t<
-      decltype(std::declval<it_reference_t<Container>>().value())>;
+      decltype(std::declval<Container&>()[0].value())>;
 
  public:
   using value_type = std::tuple<size_t, component_t<Containers>&...>;
@@ -153,15 +128,16 @@ class IndexedZipperIterator {
   using pointer = void;
   using difference_type = std::ptrdiff_t;
   using iterator_category = std::forward_iterator_tag;
-  using iterator_tuple = std::tuple<iterator_t<Containers>...>;
+  using container_tuple = std::tuple<std::reference_wrapper<Containers>...>;
 
   template <class... Cs>
   friend class IndexedZipper;
 
  private:
-  IndexedZipperIterator(iterator_tuple const& it_tuple, size_t max)
-      : m_current(it_tuple), m_max(max), m_idx(0) {
-    if (!all_set(m_seq)) {
+  IndexedZipperIterator(container_tuple const& containers, size_t max,
+                        size_t idx = 0)
+      : m_containers(containers), m_max(max), m_idx(idx) {
+    if (m_idx < m_max && !all_set(m_seq)) {
       advance_to_next_valid();
     }
   }
@@ -171,8 +147,10 @@ class IndexedZipperIterator {
   IndexedZipperIterator& operator=(const IndexedZipperIterator&) = default;
 
   IndexedZipperIterator& operator++() {
-    incr_all(m_seq);
-    advance_to_next_valid();
+    ++m_idx;
+    if (m_idx < m_max) {
+      advance_to_next_valid();
+    }
     return *this;
   }
 
@@ -198,31 +176,30 @@ class IndexedZipperIterator {
 
  private:
   template <size_t... Is>
-  void incr_all(std::index_sequence<Is...>) {
-    m_idx++;
-    (++std::get<Is>(m_current), ...);
-  }
-
-  template <size_t... Is>
   bool all_set(std::index_sequence<Is...>) {
     if (m_idx >= m_max) return false;
-    return (std::get<Is>(m_current)->has_value() && ...);
+    auto& containers_tuple = m_containers;
+    return ((m_idx < std::get<Is>(containers_tuple).get().size() &&
+             std::get<Is>(containers_tuple).get()[m_idx].has_value()) &&
+            ...);
   }
 
   template <size_t... Is>
   value_type to_value(std::index_sequence<Is...>) {
-    return std::make_tuple(m_idx,
-                           std::ref(std::get<Is>(m_current)->value())...);
+    auto& containers_tuple = m_containers;
+    return std::make_tuple(
+        m_idx,
+        std::ref(std::get<Is>(containers_tuple).get()[m_idx].value())...);
   }
 
   void advance_to_next_valid() {
     while (m_idx < m_max && !all_set(m_seq)) {
-      incr_all(m_seq);
+      ++m_idx;
     }
   }
 
  private:
-  iterator_tuple m_current;
+  container_tuple m_containers;
   size_t m_max;
   size_t m_idx;
   static constexpr std::index_sequence_for<Containers...> m_seq{};
@@ -232,32 +209,21 @@ template <class... Containers>
 class IndexedZipper {
  public:
   using iterator = IndexedZipperIterator<Containers...>;
-  using iterator_tuple = typename iterator::iterator_tuple;
+  using container_tuple = typename iterator::container_tuple;
 
   explicit IndexedZipper(Containers&... cs)
-      : m_begin(compute_begin(cs...)),
-        m_end(compute_end(cs...)),
-        m_size(compute_size(cs...)) {}
+      : m_containers(std::ref(cs)...), m_size(compute_size(cs...)) {}
 
-  iterator begin() { return iterator(m_begin, m_size); }
+  iterator begin() { return iterator(m_containers, m_size, 0); }
 
-  iterator end() { return iterator(m_end, m_size); }
+  iterator end() { return iterator(m_containers, m_size, m_size); }
 
  private:
   static size_t compute_size(Containers&... containers) {
     return std::max({containers.size()...});
   }
 
-  static iterator_tuple compute_begin(Containers&... containers) {
-    return std::make_tuple(containers.begin()...);
-  }
-
-  static iterator_tuple compute_end(Containers&... containers) {
-    return std::make_tuple(containers.end()...);
-  }
-
  private:
-  iterator_tuple m_begin;
-  iterator_tuple m_end;
+  container_tuple m_containers;
   size_t m_size;
 };
