1c6751241dcd51b14ab6cbf7a602113d5d594e51
dalia GUIZ <dalia.guiz@epitech.eu>
2025-12-04 23:59:36 +0100

    feat(gamedesign): implement enemy's component, with all the useful details

---
+++ Shared/components/Player/Enemy.hpp
@@ -0,0 +1,24 @@
+// Copyright 2025 Dalia Guiz
+#pragma once
+#include "../Physics2D.hpp"
+enum class EnemyType {
+    Basic,
+    Zigzag,
+    Wave
+};
+
+
+struct Enemy {
+    EnemyType type;
+    float speed;
+    Vector2 direction;
+    float amplitude = 20.f;
+    float timer = 0.f;
+    int scoreValue;
+    int weaponId;
+};
+
+struct Health {
+    int hp;
+    bool isAlive = true;
+};
