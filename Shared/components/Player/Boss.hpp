8a6c4e79acb2732156e90398ee022a7cf7129899
dalia GUIZ <dalia.guiz@epitech.eu>
2025-12-04 23:58:54 +0100

    feat(gamedesign): implement boss's component

---
+++ Shared/components/Player/Boss.hpp
@@ -0,0 +1,34 @@
+// Copyright 2025 Dalia Guiz
+#ifndef SHARED_COMPONENTS_PLAYER_BOSS_HPP_
+    #define SHARED_COMPONENTS_PLAYER_BOSS_HPP_
+
+#include "../Physics2D.hpp"
+enum class BossType {
+    BigShip,
+    Snake,          // Boss serpent multi-segments
+    BydoEye ,           // Boss statique qui tire beaucoup
+    Battleship,       // Boss qui spawn des ennemis
+    FinalBoss      // Ultime boss
+};
+
+enum class BossPhase {
+    Phase1,
+    Phase2,
+    Phase3
+};
+
+struct Boss {
+    BossType type;
+    BossPhase phase;
+    Vector2 direction;
+    float timer = 0.f;
+    float speed;
+    float amplitude = 40.f;
+};
+
+struct Health {
+    float hp = 100.f;
+    float maxHp = 100.f;
+};
+
+#endif  // SHARED_COMPONENTS_PLAYER_BOSS_HPP_
