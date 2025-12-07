1ca4e3e65313901b6fb30feb3af933280d21631a
jeremy <jeremy.armijo@epitech.eu>
2025-12-07 18:16:55 +0100

    feat(network): add udp management server in r-type server

---
+++ Server/include/network/UDPServer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <asio.hpp>
+#include <array>
+#include <functional>
+#include <memory>
+
+class UDPServer {
+public:
+    using ReceiveCallback = std::function<void(
+        const std::vector<uint8_t>&,
+        const asio::ip::udp::endpoint&
+    )>;
+
+    UDPServer(asio::io_context& io_context, uint16_t port);
+    ~UDPServer();
+
+    void SetReceiveCallback(ReceiveCallback callback);
+    void SendTo(const std::vector<uint8_t>& data, 
+                const asio::ip::udp::endpoint& endpoint);
+    
+    bool IsOpen() const { return socket_.is_open(); }
+    void Close();
+
+private:
+    void StartReceive();
+    void HandleReceive(const asio::error_code& error, std::size_t bytes_transferred);
+
+    asio::ip::udp::socket socket_;
+    asio::ip::udp::endpoint remote_endpoint_;
+    std::array<uint8_t, 1400> recv_buffer_;
+    ReceiveCallback receive_callback_;
+};
