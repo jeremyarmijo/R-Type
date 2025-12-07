18e4535ccbfdac6d90595a779e66b83c7d758710
jeremy <jeremy.armijo@epitech.eu>
2025-12-07 18:03:35 +0100

    feat(network): add abstract for server management with networkmanagement

---
+++ Server/NetworkMessage.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <cstdint>
+
+#include <asio.hpp>
+
+using UDPEndpoint = asio::ip::udp::endpoint;
+using ErrorCode = asio::error_code;
+
+struct NetworkMessage {
+    uint32_t client_id;              // 0 pour serveur
+    std::vector<uint8_t> data;
+    uint64_t timestamp;              // Pour latence/ordering
+    
+    NetworkMessage() : client_id(0), timestamp(0) {}
+};
