7375d8d2cc4b0122c1ea437fbeb65f6f50e3a7ab
ZiadBengherabi <ziad.bengherabi@epitech.eu>
2025-12-10 13:43:45 +0100

    fix(projectile): update variable name from is_active to isActive for consistency

--- Shared/components/Player/Projectile.hpp
+++ Shared/components/Player/Projectile.hpp
@@ -10,8 +10,8 @@ struct Projectile {
   float lifetime;
   float currentLife;
   size_t ownerId;
-  bool is_active = true;
+  bool isActive = true;
 
   explicit Projectile(float dmg = 10.0f, float spd = 300.0f, Vector2 dir = {1.0f, 0.0f}, float life = 5.0f, size_t owner = 0)
-      : damage(dmg), speed(spd), direction(dir), lifetime(life), currentLife(0.0f), ownerId(owner), is_active(true) {}
+      : damage(dmg), speed(spd), direction(dir), lifetime(life), currentLife(0.0f), ownerId(owner), isActive(true) {}
 };
