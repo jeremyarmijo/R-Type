36ab95a41fb773ca55c6fd2f5bbe4c8dec0a93a3
Xavier Daste <xavier.daste@epitech.eu>
2025-12-14 21:15:26 +0100

    feat(engine): changed playerindex vector selection to a map to avoid crashes when inserting unordered player indexs

--- Client/src/engine/settings/MultiplayerSkinManager.hpp
+++ Client/src/engine/settings/MultiplayerSkinManager.hpp
@@ -1,58 +1,105 @@
 #pragma once
 #include <iostream>
+#include <unordered_map>
 #include <unordered_set>
-#include <vector>
-
 #include "settings/PlayerSettings.hpp"
 
 class MultiplayerSkinManager {
- private:
+private:
   PlayerSkin m_localPlayerSkin;
-  std::vector<PlayerSkin> m_otherPlayerSkins;
+  uint16_t m_localPlayerId;
+  std::unordered_map<uint16_t, PlayerSkin> m_playerSkins;
   std::unordered_set<int> m_usedSkins;
 
- public:
-  MultiplayerSkinManager() : m_localPlayerSkin(PlayerSkin::BLUE) {}
+public:
+  MultiplayerSkinManager() 
+    : m_localPlayerSkin(PlayerSkin::BLUE),
+      m_localPlayerId(0) {}
 
-  void SetLocalPlayerSkin(PlayerSkin skin) {
+  void SetLocalPlayerSkin(PlayerSkin skin, uint16_t playerId) {
     m_localPlayerSkin = skin;
+    m_localPlayerId = playerId;
     m_usedSkins.clear();
     m_usedSkins.insert(static_cast<int>(skin));
+    
+    std::cout << "Local player (ID: " << playerId << ") using skin: " 
+              << PlayerSettings::GetSkinName(skin) << std::endl;
   }
 
-  PlayerSkin AssignSkinForPlayer(int playerIndex) {
-    if (playerIndex < static_cast<int>(m_otherPlayerSkins.size())) {
-      return m_otherPlayerSkins[playerIndex];
+  PlayerSkin AssignSkinForPlayer(uint16_t playerId) {
+    if (playerId == m_localPlayerId) {
+      return m_localPlayerSkin;
+    }
+
+    auto it = m_playerSkins.find(playerId);
+    if (it != m_playerSkins.end()) {
+      return it->second;
     }
 
     PlayerSkin assignedSkin = FindUnusedSkin();
-    if (playerIndex >= static_cast<int>(m_otherPlayerSkins.size())) {
-      m_otherPlayerSkins.resize(playerIndex + 1);
-    }
-
-    m_otherPlayerSkins[playerIndex] = assignedSkin;
+    m_playerSkins[playerId] = assignedSkin;
     m_usedSkins.insert(static_cast<int>(assignedSkin));
 
     std::cout << "Assigned skin " << PlayerSettings::GetSkinName(assignedSkin)
-              << " to player " << playerIndex << std::endl;
+              << " to player ID " << playerId << std::endl;
 
     return assignedSkin;
   }
 
-  void RemovePlayer(int playerIndex) {
-    if (playerIndex < static_cast<int>(m_otherPlayerSkins.size())) {
-      int skinInt = static_cast<int>(m_otherPlayerSkins[playerIndex]);
+  bool HasPlayer(uint16_t playerId) const {
+    return m_playerSkins.find(playerId) != m_playerSkins.end();
+  }
+
+  PlayerSkin GetPlayerSkin(uint16_t playerId) const {
+    if (playerId == m_localPlayerId) {
+      return m_localPlayerSkin;
+    }
+
+    auto it = m_playerSkins.find(playerId);
+    if (it != m_playerSkins.end()) {
+      return it->second;
+    }
+
+    return PlayerSkin::BLUE;
+  }
+
+  void RemovePlayer(uint16_t playerId) {
+    auto it = m_playerSkins.find(playerId);
+    if (it != m_playerSkins.end()) {
+      int skinInt = static_cast<int>(it->second);
       m_usedSkins.erase(skinInt);
+      m_playerSkins.erase(it);
+
+      std::cout << "Removed player ID " << playerId 
+                << ", freed skin for reuse" << std::endl;
     }
   }
 
   void Clear() {
-    m_otherPlayerSkins.clear();
+    m_playerSkins.clear();
     m_usedSkins.clear();
     m_usedSkins.insert(static_cast<int>(m_localPlayerSkin));
+
+    std::cout << "Cleared all player skin assignments" << std::endl;
   }
 
- private:
+  size_t GetPlayerCount() const {
+    return m_playerSkins.size();
+  }
+
+  void PrintDebug() const {
+    std::cout << "\n=== Skin Manager Debug ===" << std::endl;
+    std::cout << "Local Player (ID: " << m_localPlayerId << "): " 
+              << PlayerSettings::GetSkinName(m_localPlayerSkin) << std::endl;
+    std::cout << "Other Players:" << std::endl;
+    for (const auto& [playerId, skin] : m_playerSkins) {
+      std::cout << "  Player " << playerId << ": " 
+                << PlayerSettings::GetSkinName(skin) << std::endl;
+    }
+    std::cout << "========================\n" << std::endl;
+  }
+
+private:
   PlayerSkin FindUnusedSkin() {
     for (int i = 0; i < 5; ++i) {
       if (m_usedSkins.find(i) == m_usedSkins.end()) {
@@ -60,8 +107,6 @@ class MultiplayerSkinManager {
       }
     }
 
-    // If all skins are used (more than 5 players), just cycle through
-    // This shouldn't happen with 5 skins and max 4 players, but just in case
-    return static_cast<PlayerSkin>(m_otherPlayerSkins.size() % 5);
+    return static_cast<PlayerSkin>(m_playerSkins.size() % 5);
   }
 };
