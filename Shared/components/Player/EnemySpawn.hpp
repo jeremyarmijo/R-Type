a0f9f7aeb29cce21c1867335ed0d4e0cf8467cf6
dalia GUIZ <dalia.guiz@epitech.eu>
2025-12-14 16:28:55 +0100

    feat(spawn): add enemyspawning component to manage enemy spawn enemies

---
+++ Shared/components/Player/EnemySpawn.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+
+#include "./Enemy.hpp"
+
+struct EnemySpawning {
+  std::vector<Vector2> spawnPoints;
+  float spawnInterval;
+  float timer = 0.0f;
+  int maxEnemiesAct;
+  EnemyType type;
+  int spawnAmount;
+  bool waveActive = false;
+};
