ccdc636731e78315b5ecad0293aa3446845cc0e6
dalia GUIZ <dalia.guiz@epitech.eu>
2025-12-30 13:53:58 +0100

    feat(gamedesign): implement a levelcomponent to structure diffrent  part of a level

---
+++ Shared/components/Levels.hpp
@@ -0,0 +1,29 @@
+// components/Levels.hpp
+#pragma once
+#include <optional>
+#include <vector>
+
+#include "components/Physics2D.hpp"
+#include "Player/Boss.hpp"
+#include "Player/Enemy.hpp"
+
+struct Wave {
+  std::vector<EnemyType> enemyTypes;
+  std::vector<int> enemiesPerType;
+  std::vector<Vector2> spawnPositions;
+  bool isBossWave = false;
+  std::optional<BossType> bossType;
+  int bossHP = 0;
+};
+
+struct LevelComponent {
+  std::vector<Wave> waves;
+  int currentWave = -1;
+  float waveDelayTimer = 0.0f;
+  bool finishedLevel = false;
+  bool initialized = false;
+
+  LevelComponent() = default;
+  explicit LevelComponent(const std::vector<Wave>& ws)
+  : waves(ws), currentWave(-1) {}
+};
